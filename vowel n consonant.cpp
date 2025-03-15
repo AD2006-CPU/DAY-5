1912d8ef0d4114c45152c29f441c6c147dc28bef
AD2006-CPU <anushkadey424@gmail.com>
2025-03-15 17:52:48 +0530

    Add files via upload

---
+++ vowel n consonant.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() 
+{
+    char x;
+    printf("Enter x = ");
+    scanf("%c", &x);
+    if (x == 'a') 
+	{
+    printf("VOWEL");
+    } 
+    else if (x == 'e')
+	{
+	printf("VOWEL");
+	}
+	else if (x == 'i')
+	{
+	printf("VOWEL");
+	}
+	else if (x == 'o')
+    {
+    printf("VOWEL");
+    }   
+    else if (x == 'u')
+    {
+    printf("VOWEL");
+	}  
+	else
+	{
+	printf("CONSONANT");
+	}
+	return 0;
+}
+
